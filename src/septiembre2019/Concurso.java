31db1f71b4d207ecf546f1d2c4a41831799c7bfb
CGPerezAranda <cg.perezaranda@uma.es>
2022-05-22 12:24:13 +0200

    Esqueletos septiembre 2020 y 2019

---
+++ src/septiembre2019/Concurso.java
@@ -0,0 +1,17 @@
+package septiembre2019;
+
+
+
+public class Concurso {
+
+	
+	public void tirarMoneda(int id,boolean cara) throws InterruptedException {
+		
+		
+	}	
+	
+	public boolean concursoTerminado() {
+		
+		return false; //borrar
+	}
+}
